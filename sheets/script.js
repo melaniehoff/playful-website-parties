37645acd440dc58eea5d957bac552e68ffe5b40a
CommonMatter <hoffmelanie@gmail.com>
2022-11-08 15:07:15 -0500

    iterating over list of obj

--- sheets/script.js
+++ sheets/script.js
@@ -9,15 +9,29 @@ function init() {
           complete: function(results) {
             var data = results.data
             console.log(data)
-            console.log(data[0].Timestamp)
 
-            var options = {
-                element: document.getElementById("table"),
-                data: data
-            };
+            // Put every timestamp in a div
+            for (var entry of data) {
+                console.log(entry.Timestamp);
+                $( ".my-fun-class" ).append( "<p>"+ entry.Timestamp +"</p>" );
+            }
 
-            var table = new Table(options);
-            table.view();
+            // Put every response to the invitation question in a div
+            for (var entry of data) {
+                //note that it's not as simple to address this value becuase the name of it is not very machine readable, unline "Timestamp." The name of it is "What ways do you like to invite someone to do something with you?." This can't be used in the same way becuase of all the spaces and punctuation so we need to address it by referring to is as the second key of each object (remember second looks like [1]) for each entry object.
+                var invitationResponse = entry[Object.keys(entry)[1]]
+                console.log(invitationResponse)
+                $( ".my-fun-class" ).append( "<p>"+ invitationResponse +"</p>" );
+            }
+
+            //// This is the original table code that displays the data similar to the way the spreadsheet looks
+            // var options = {
+            //     element: document.getElementById("table"),
+            //     data: data
+            // };
+            //
+            // var table = new Table(options);
+            // table.view();
 
           }
         })
